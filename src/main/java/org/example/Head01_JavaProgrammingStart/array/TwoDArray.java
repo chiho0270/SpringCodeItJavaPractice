555d839ded672b6cc31c806b2272daecd923c712
chiho0270 <ichiho9687@gmail.com>
2025-08-08 16:21:24 +0900

    Array 실습 추가 (2)

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/array/TwoDArray.java
@@ -0,0 +1,26 @@
+package org.example.Head01_JavaProgrammingStart.array;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12}
+        };
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+}
