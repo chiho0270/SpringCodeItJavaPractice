8dd62985d4802432a1f5635716256e89b71bf251
chiho0270 <ichiho9687@gmail.com>
2025-08-12 11:24:40 +0900

    Add. oop example02

--- src/main/java/org/example/Head02_OOPprogramming/topic03_OOPConcept/example02/AbstactPayment.java
+++
@@ -1,4 +0,0 @@
-package org.example.Head02_OOPprogramming.topic03_OOPConcept.example02;
-
-public class AbstactPayment {
-}
--- src/main/java/org/example/Head02_OOPprogramming/topic03_OOPConcept/example02/AccountTransferPayment.java
+++ src/main/java/org/example/Head02_OOPprogramming/topic03_OOPConcept/example02/AccountTransferPayment.java
@@ -1,16 +1,14 @@
 package org.example.Head02_OOPprogramming.topic03_OOPConcept.example02;
 
-public class CreditCardPayment extends AbstractPayment {
+public class AccountTransferPayment extends AbstractPayment {
     @Override
     public void pay(double amount) {
         validatePayment(amount);
-        System.out.println("신용카드 결제 완료: " + amount + "원");
+        System.out.println("계좌이체 결제 완료: " + amount + "원");
     }
-
     @Override
     public void cancel(double amount) {
         validateCancel(amount);
-        System.out.println("신용카드 결제 취소: " + amount + "원");
+        System.out.println("계좌이체 결제 취소: " + amount + "원");
     }
 }
-
