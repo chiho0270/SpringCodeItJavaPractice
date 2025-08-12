1da6cc9cd9e929f1807fa91e2c1afe0646ae68a9
chiho0270 <ichiho9687@gmail.com>
2025-08-12 14:32:55 +0900

    end next step.

--- src/main/java/org/example/Head02_OOPprogramming/topic03_OOPConcept/practice_payment/AbstractPayment.java
+++ src/main/java/org/example/Head02_OOPprogramming/topic03_OOPConcept/practice_payment/AbstractPayment.java
@@ -1,8 +1,8 @@
 package org.example.Head02_OOPprogramming.topic03_OOPConcept.practice_payment;
 
-// 인터페이스로 정의된 Payment를 AbstractPayment로 구현
+// 인터페이스로 정의된 Payment를 AbstractPayment로 구현하겠다
 public abstract class AbstractPayment implements Payment{
-    // 추상 메소드 : 구현 X, 각 상속받은(자식) 클래스에 구체해줘야 함
+    // 추상 메소드 : 추상 클래스에서 구체 X, 각 상속받은(자식) 클래스에 구체해줘야 함
     public abstract void pay(double amount);
 
     // 구체 메소드 : 해당 추상 클래스의 상속받는(자식) 클래스에 공통되는 클래스
